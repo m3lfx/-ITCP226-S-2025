c2e57ff73a5485c8d998b4e88765bf9fc9d19021
rommel dalisay <rommelmdalisay@gmail.com>
2025-02-06 11:09:51 +0800

    item store

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
@@ -27,7 +28,13 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request);
+        Item::create([
+            'description' => trim($request->description),
+            'cost_price' => trim($request->cost_price),
+            'sell_price' => trim($request->cost_price)
+        ]);
+        return 'item added';
     }
 
     /**
