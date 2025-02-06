c35108f109a2b18d045de1ec5724c71841781592
rommel dalisay <rommelmdalisay@gmail.com>
2025-02-06 10:53:40 +0800

    add item

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -3,6 +3,8 @@
 @section('body')
     <div class="container">
         {!! Form::open(['route' => 'items.store']) !!}
+        {!! Form::label('desc', 'item name', ['class' => 'form-label']) !!}
+        {!! Form::text('description', null, ['class' => 'form-control', 'id' => 'desc']) !!}
         {!! Form::close() !!}
     </div>
 @endsection
