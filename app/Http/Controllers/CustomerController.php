f58a316b3c7e895d235c7da3e81fe471df1bc983
rommel dalisay <rommelmdalisay@gmail.com>
2025-05-08 14:01:06 +0800

    many to many

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use App\Models\User;
+use App\Models\Order;
 
 class CustomerController extends Controller
 {
@@ -17,10 +18,21 @@ class CustomerController extends Controller
         // dd($customer);
         // $user = Customer::find(1)->user;
         // dd($user);
-        $orders = Customer::find(1)->orders;
-        // dd($orders);
-        foreach($orders as $order) {
-            dump($order->orderinfo_id, $order->date_placed);
+        // $orders = Customer::find(1)->orders;
+        // // dd($orders);
+        // foreach($orders as $order) {
+        //     dump($order->orderinfo_id, $order->date_placed);
+        // }
+
+        // $customer = Order::find(2)->customer;
+        // dd($customer);
+        // foreach($orders as $order) {
+        //     dump($order->orderinfo_id, $order->date_placed);
+        // }
+        $items = Order::find(1)->items;
+        // dd($items);
+        foreach($items as $item) {
+            dump($item->description);
         }
     }
 
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,4 +11,14 @@ class Order extends Model
     protected $table = 'orderinfo';
     protected $primaryKey = 'orderinfo_id';
     public $timestamps = false;
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class, 'orderline', 'orderinfo_id', 'item_id');
+    }
 }
